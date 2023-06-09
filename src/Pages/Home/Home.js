9eb3135a28565dd3ab15c889ccbda3c2fd945992
janvi_kakadiya <janvikakadiya716@gmail.com>
2023-06-09 19:27:18 +0530

    mobile slider

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,66 +5,15 @@ import Category from '../../Components/Category/Category';
 import Racipes_category from '../Recipes/Racipes_category';
 import Home_slider from '../../Components/Home_slider/Home_slider';
 import CategoryButton from '../../Components/Buttons/Category_btn';
+import Mobile_slider from '../../Components/Home_slider/Mobile_slider';
 
 function Home() {
   return (
     <>
-      {/* home */}
+    
       <div className="home">
         <Home_slider />
-        {/* main-slider */}
-        
-        {/* end of main-slider */}
-        {/* mobile-slider */}
-        
-        <div className="container-fluid mobile_view_home">
-  <div
-    id="carouselExampleInterval"
-    className="carousel slide"
-    data-bs-ride="carousel"
-  >
-    <div className="carousel-inner">
-      <div
-        className="carousel-item active"
-        data-bs-interval="3000"
-      >
-        <img src="./slide.png" className="d-block w-100 slide_img" alt="Slide" />
-        <div className="carousel-caption">
-          <h1 className="text-dark slide_mobile">Spicy delicious chicken wings</h1>
-        </div>
-      </div>
-      <div
-        className="carousel-item"
-        data-bs-interval="3000"
-      >
-        <img src="./slide.png" className="d-block w-100 slide_img" alt="Slide" />
-        <div className="carousel-caption">
-          <h1 className="text-dark slide_mobile">Spicy delicious chicken wings</h1>
-        </div>
-      </div>
-    </div>
-    <button
-      className="carousel-control-prev"
-      type="button"
-      data-bs-target="#carouselExampleInterval"
-      data-bs-slide="prev"
-    >
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Previous</span>
-    </button>
-    <button
-      className="carousel-control-next"
-      type="button"
-      data-bs-target="#carouselExampleInterval"
-      data-bs-slide="next"
-    >
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Next</span>
-    </button>
-  </div>
-</div>
-
-        {/* end of mobile slider */}
+       <Mobile_slider/>
         <Category />
         <Racipes_category />
         {/* poster chef */}
